f21682c3aeecec9ea25ef17937d6f56fa7f1bae7
Ilker Guller <694940+Sly777@users.noreply.github.com>
2024-02-26 14:54:51 +0100

    added immersivespace example

--- visionos/MLVisionRN/App.swift
+++ visionos/MLVisionRN/App.swift
@@ -6,8 +6,23 @@ import React_RCTSwiftExtensions
 struct MLVisionRNApp: App {
   @UIApplicationDelegateAdaptor var delegate: AppDelegate
 
+  @State private var orbitImmersionStyle: ImmersionStyle = .mixed
+  @State private var solarImmersionStyle: ImmersionStyle = .full
+
   var body: some Scene {
     RCTMainWindow(moduleName: "MLVisionRN")
       .defaultSize(CGSize(width: 1280, height: 720))
+
+    /* ImmersiveSpace(id: Module.orbit.name) {
+        Orbit()
+            .environment(model)
+    }
+    .immersionStyle(selection: $orbitImmersionStyle, in: .mixed)
+
+    ImmersiveSpace(id: Module.solar.name) {
+        SolarSystem()
+            .environment(model)
+    }
+    .immersionStyle(selection: $solarImmersionStyle, in: .full) */
   }
 }
